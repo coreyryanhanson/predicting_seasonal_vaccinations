3aaa0d407c9e2b1104fb6e4692d1f6844b0f95b5
Corey <corey@localhost.localdomain>
2020-04-14 16:05:31 -0400

    establishing workflow

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
 
 def categorical_nans(df, value):
     """Goes through a dataframe (or selection) and replaces all nans with a single value."""
@@ -6,3 +8,30 @@ def categorical_nans(df, value):
     for column in df.columns:
         df[column].fillna(value, inplace=True)
     return df
+
+def get_imputer_objs(df, impute_dict):
+    """Stores a list of fitted imputers to transform the data later."""
+
+    return [SimpleImputer(val[0], val[1], val[2]).fit(df[[key]]) for key, val in impute_dict.items()]
+
+
+def impute_vals(df, impute_dict, imputers):
+    """"Loops through imputer object list and creates dummies for missing values. If the dictionary indicates that
+    a new categorical value should be added, dummy creation is skipped."""
+
+    for i, column in enumerate(impute_dict.keys()):
+        if type(impute_dict[column][2]) != str and impute_dict[column][1] != "constant":
+            df["missing_"+ column] = missing_val_dummies(df, column, impute_dict[column][0])
+        index = df[column].index
+        df[column] = pd.Series(imputers[i].transform(df[[column]]).ravel(), index=index)
+    return df
+
+
+def missing_val_dummies(df, column, bad_data):
+    """Creates a new column containing dummy variables indicating what was missing when imputing data."""
+
+    if np.isnan(bad_data):
+        return np.where(df[column].isna().values == True, 1, 0)
+    else:
+        return np.where(df[column].values == bad_data, 1, 0)
+
