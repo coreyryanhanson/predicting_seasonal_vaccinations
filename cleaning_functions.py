885e6726bdf45b990b99dd77280a524f7b6bf74d
Corey <corey@localhost.localdomain>
2020-04-14 17:31:17 -0400

    consoildated cleaning

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -1,5 +1,7 @@
+import re
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 from sklearn.impute import SimpleImputer
 
 def categorical_nans(df, value):
@@ -35,3 +37,23 @@ def missing_val_dummies(df, column, bad_data):
     else:
         return np.where(df[column].values == bad_data, 1, 0)
 
+def rename_ages(x):
+    if x == '65+ Years':
+        new = "Over_65"
+    else:
+        new = re.sub("\s-\s", "_to_", x)
+    return new
+
+
+def fit_clean_data(df, impute_dict):
+    # Fits imputers
+
+    imputes = get_imputer_objs(df, impute_dict)
+    return clean_data(df, imputes, impute_dict), imputes
+
+def clean_data(df, imputes, impute_dict):
+
+    df = impute_vals(df, impute_dict, imputes)
+    df = categorical_nans(df, "unknown")
+    df["age_group"] = df["age_group"].map(rename_ages)
+    return df
